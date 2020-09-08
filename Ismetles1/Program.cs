eefecee966beb8e770774ea40afe0a009674180c
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-08 08:09:38 +0200

    Add project files.

---
+++ Ismetles1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ismetles1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
