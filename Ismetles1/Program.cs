c13d4eeb58921a425a544d4dcc9068e4a2584d47
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-09 08:34:02 +0200

    A játék készen van.

--- Ismetles1/Program.cs
+++ Ismetles1/Program.cs
@@ -7,6 +7,11 @@ namespace Ismetles1
     {
         static void Main(string[] args)
         {
+            bool dontes = true;
+
+            while (dontes)
+            {
+
             string[] lehetoseg = new string[] {"Kő", "Papír", "Olló"} ;
 
             Random vel = new Random();
@@ -21,9 +26,70 @@ namespace Ismetles1
 
             Console.Write("Válassz: "); jatekosValasz = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Játékos választása: {lehetoseg[jatekosValasz]}");
-
-            Console.ReadKey();
+                if (jatekosValasz < 3)
+                {
+                    Console.WriteLine($"Játékos választása: {lehetoseg[jatekosValasz]}");
+                    switch (jatekosValasz)
+                    {
+                        default:
+                            break;
+                        case 0:
+                            if (gepValasz == 0)
+                            {
+                                Console.WriteLine("A gép is követ válaszott, döntetlen.");
+                            }
+                            else if (gepValasz == 1)
+                            {
+                                Console.WriteLine("A gép papírt válaszott, vesztettél.");
+                            }
+                            else if (gepValasz == 2)
+                            {
+                                Console.WriteLine("A gép ollót választott, nyertél.");
+                            }
+                            break;
+                        case 1:
+                            if (gepValasz == 0)
+                            {
+                                Console.WriteLine("A gép követ válaszott, nyertél.");
+                            }
+                            else if (gepValasz == 1)
+                            {
+                                Console.WriteLine("A gép is papírt válaszott, döntetlen.");
+                            }
+                            else if (gepValasz == 2)
+                            {
+                                Console.WriteLine("A gép ollót választott, vesztettél.");
+                            }
+                            break;
+                        case 2:
+                            if (gepValasz == 0)
+                            {
+                                Console.WriteLine("A gép követ válaszott, vesztettél.");
+                            }
+                            else if (gepValasz == 1)
+                            {
+                                Console.WriteLine("A gép papírt válaszott, nyertél.");
+                            }
+                            else if (gepValasz == 2)
+                            {
+                                Console.WriteLine("A gép is ollót választott, döntetlen.");
+                            }
+                            break;
+                    }
+                    Console.Write($"Szeretnél még játszani? I/N {" "}");
+                    char valasz;
+                    valasz = char.Parse(Console.ReadLine());
+                    if (valasz == 'N')
+                    {
+                        dontes = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Rossz számot írtál, a program bezár.");
+                    dontes = false;
+                }
+            }
         }
     }
 }
